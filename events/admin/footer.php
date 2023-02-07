ba9648770aedc9ac6b285265489031bb80bd5b06
ShubhamAnand123 <98607487+ShubhamAnand123@users.noreply.github.com>
2023-02-08 04:08:26 +0530

    Add files via upload

---
+++ events/admin/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2023 EVENTS | MADE BY- by SHUBHAM RJX CHANDAN</span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+</body>
+</html>
